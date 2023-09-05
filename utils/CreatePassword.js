6a6f7fa2b5be8ef2becc4e16bdb2a4c5db17ac0f
SheheryarGhaznavi <sheheryarghaznavi@yahoo.com>
2023-09-05 13:19:49 +0500

    random password generation logic completed

--- utils/CreatePassword.js
+++ utils/CreatePassword.js
@@ -8,7 +8,19 @@ const createPassword = (length = 8, has_numbers = true, has_symbols = true) => {
     has_numbers ? chars += numbers : '';
     has_symbols ? chars += symbols : '';
 
-    return chars;
+    return generatePassword(length, chars);
+};
+
+
+function generatePassword(length, chars)
+{
+    let password = '';
+
+    for (let index = 0; index < length; index++) {
+        password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+
+    return password;
 }
 
 module.exports = createPassword;
